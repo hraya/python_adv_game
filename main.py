c6fc9be78878db9636bf73de978814c024406cab
hraya <hraya@lssu.edu>
2019-08-23 20:50:13 -0500

    Added comment

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ player1 = Person("Valos:", 3260, 132, 300, 34, player_spells, player_items)
 player2 = Person("Nick :", 4160, 188, 311, 34, player_spells, player_items)
 player3 = Person("Robot:", 3089, 174, 288, 34, player_spells, player_items)
 
-
+# Instantiate Enemies
 enemy1 = Person("Imp  ", 1250, 130, 560, 325, enemy_spells, [])
 enemy2 = Person("Magus", 11200, 701, 525, 25, enemy_spells, [])
 enemy3 = Person("Imp  ", 1250, 130, 560, 325, enemy_spells, [])
